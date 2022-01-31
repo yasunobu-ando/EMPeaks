9124be086cec92a4c23f50b82bb95f33608342ff
Yasunobu Andi <yasunobu.ando@aist.go.jp>
2022-01-31 11:01:07 +0900

    Implementing XAFS background, Tanh, TanhLinear, TanhQuad, TanhCubic.

--- EMPeaks/Background/__init__.py
+++ EMPeaks/Background/__init__.py
@@ -1,6 +1,8 @@
 from ._background import UniformModel, SquareRootModel, \
-                         LinearModel, TriangleModel, RampModel
+                         LinearModel, TriangleModel, RampModel, \
+                         TanhModel, TanhLinearModel, TanhQuadModel, TanhCubicModel
 
 __all__ = ['UniformModel', 'SquareRootModel',
-           'LinearModel', 'RampModel', 'TriangleModel']
+           'LinearModel', 'RampModel', 'TriangleModel',
+           'TanhModel', 'TanhLinearModel', 'TanhQuadModel', 'TanhCubicModel']
 
--- EMPeaks/Background/_background.py
+++ EMPeaks/Background/_background.py
@@ -1,5 +1,6 @@
 import numpy as np
-from scipy import stats, optimize #, integrate
+from scipy import stats, optimize
+from scipy.integrate import quad
 
 
 class UniformModel:
@@ -22,77 +23,6 @@ class UniformModel:
 
     def maximum_likelihood_estimation(self, x, weight):
         return
-
-
-class SquareRootModel:
-    """
-    SquareRootModel(self, x0, x1):
-    x: sampling point or data bins
-    w: weight or intensity of the data
-    """
-    def __init__(self, x0, x1):
-        self.x0 = x0
-        self.x1 = x1
-
-    def predict(self, x):
-        return np.sqrt(x) /(2.0/3.0 * (self.x1**(1.5) - self.x0**(1.5)))
-
-    def _LL(self, x, weight):
-        t = np.log(self.predict(x))
-        self.LL = (t * weight).sum()
-        return self.LL
-
-    def maximum_likelihood_estimation(self, x, weight):
-        return
-
-
-class LinearModelEM:
-    """
-    UniformModel(self, x, w):
-    x: sampling point or data bins
-    w: weight or intensity of the data
-    """
-    def __init__(self, x0, x1):
-        self.x0 = x0
-        self.width = x1 - x0
-        self.pi_uni = stats.uniform.rvs(0, 1, 1)
-        self.pi_tri = 1.0 - self.pi_uni
-        self.LL = 0.0
-
-    def predict(self, x):
-        return self.pi_tri * 2.0 * stats.triang.pdf(x, 0.5, loc = self.x0, scale=2*self.width) \
-                + self.pi_uni * stats.uniform.pdf(x, loc=self.x0, scale=self.width)
-
-    def _LL(self, x, weight):
-        t = np.log(self.predict(x))
-        self.LL = (t * weight).sum()
-        return self.LL
-
-    def maximum_likelihood_estimation(self, x, weight):
-        eps = 1.0e-3
-        max_iter = 10000
-        ll = [self._LL(x, weight)]
-        for i in range(max_iter):
-            self._e_step(x)
-            self._m_step(x, weight)
-            ll.append(self._LL(x, weight))
-            #print(ll[-1], ll[-1] - ll[-2])
-            if np.abs(ll[-1] - ll[-2]) < eps:
-                #print(self.pi_uni, self.pi_tri)
-                return
-        print("Warning!!! LinearBackground does not converge.")
-        print(self.pi_uni, self.pi_tri)
-        return
-
-    def _e_step(self, x):
-        self._gamma_tri = self.pi_tri * 2.0 * stats.triang.pdf(x, 0.5, loc=self.x0,
-                                                               scale=2 * self.width) / self.predict(x)
-        self._gamma_uni = self.pi_uni * stats.uniform.pdf(x, loc=self.x0,
-                                                          scale=self.width) / self.predict(x)
-
-    def _m_step(self, x, weight):
-        self.pi_tri = np.sum(self._gamma_tri * weight) / np.sum(weight)
-        self.pi_uni = np.sum(self._gamma_uni * weight) / np.sum(weight)
 
 
 class LinearModel:
@@ -184,3 +114,219 @@ class TriangleModel:
 
     def maximum_likelihood_estimation(self, x, weight):
         return
+
+class SquareRootModel:
+    """
+    SquareRootModel(self, x0, x1):
+    x: sampling point or data bins
+    w: weight or intensity of the data
+    """
+    def __init__(self, x0, x1):
+        self.x0 = x0
+        self.x1 = x1
+
+    def predict(self, x):
+        return np.sqrt(x) /(2.0/3.0 * (self.x1**(1.5) - self.x0**(1.5)))
+
+    def _LL(self, x, weight):
+        t = np.log(self.predict(x))
+        self.LL = (t * weight).sum()
+        return self.LL
+
+    def maximum_likelihood_estimation(self, x, weight):
+        return
+
+
+class LinearModelEM:
+    """
+    UniformModel(self, x, w):
+    x: sampling point or data bins
+    w: weight or intensity of the data
+    """
+    def __init__(self, x0, x1):
+        self.x0 = x0
+        self.width = x1 - x0
+        self.pi_uni = stats.uniform.rvs(0, 1, 1)
+        self.pi_tri = 1.0 - self.pi_uni
+        self.LL = 0.0
+
+    def predict(self, x):
+        return self.pi_tri * 2.0 * stats.triang.pdf(x, 0.5, loc = self.x0, scale=2*self.width) \
+                + self.pi_uni * stats.uniform.pdf(x, loc=self.x0, scale=self.width)
+
+    def _LL(self, x, weight):
+        t = np.log(self.predict(x))
+        self.LL = (t * weight).sum()
+        return self.LL
+
+    def maximum_likelihood_estimation(self, x, weight):
+        eps = 1.0e-3
+        max_iter = 10000
+        ll = [self._LL(x, weight)]
+        for i in range(max_iter):
+            self._e_step(x)
+            self._m_step(x, weight)
+            ll.append(self._LL(x, weight))
+            #print(ll[-1], ll[-1] - ll[-2])
+            if np.abs(ll[-1] - ll[-2]) < eps:
+                #print(self.pi_uni, self.pi_tri)
+                return
+        print("Warning!!! LinearBackground does not converge.")
+        print(self.pi_uni, self.pi_tri)
+        return
+
+    def _e_step(self, x):
+        self._gamma_tri = self.pi_tri * 2.0 * stats.triang.pdf(x, 0.5, loc=self.x0,
+                                                               scale=2 * self.width) / self.predict(x)
+        self._gamma_uni = self.pi_uni * stats.uniform.pdf(x, loc=self.x0,
+                                                          scale=self.width) / self.predict(x)
+
+    def _m_step(self, x, weight):
+        self.pi_tri = np.sum(self._gamma_tri * weight) / np.sum(weight)
+        self.pi_uni = np.sum(self._gamma_uni * weight) / np.sum(weight)
+
+
+class TanhModel:
+    def __init__(self, x_min=-10.0, x_max=10.0):
+        self.x_min = x_min
+        self.x_max = x_max
+        self.x0 = 0.0
+        self.a = 2.0
+        self.eps = 1.0e-10
+
+    def normalization_factor(self):
+        return 1 / self.a * (+ np.log(np.cosh(self.a * (self.x_max - self.x0)))
+                             - np.log(np.cosh(self.a * (self.x_min - self.x0)))) + (self.x_max - self.x_min)
+
+    def predict(self, x):
+        Z = self.normalization_factor()
+        y = (np.tanh(self.a * (x - self.x0)) + 1.0)
+        return y / Z
+
+    def log_likelihood(self, x, intensity):
+        return np.sum(intensity * np.log(self.predict(x) + self.eps))
+
+    def MLE(self, x, intensity):
+        def func(param):
+            self.x0 = param[0]
+            self.a = param[1]
+            return - self.log_likelihood(x, intensity)
+
+        init = [self.x0, self.a]
+        info = optimize.minimize(func, x0=init, bounds=[(self.x_min, self.x_max),
+                                                        (0, np.inf)], method='L-BFGS-B')
+        self.x0 = info['x'][0]
+        self.a = info['x'][1]
+
+        return info
+
+
+class TanhLinearModel:
+    def __init__(self, x_min=-10.0, x_max=10.0):
+        self.x_min = x_min
+        self.x_max = x_max
+        self.x0 = 0.0
+        self.a = 2.0
+        self.eps = 1e-10
+
+    def normalization_factor(self):
+        def func(x):
+            return -(x - self.x_max) * (np.tanh(self.a * (x - self.x0)) + 1.0)
+
+        return quad(func, self.x_min, self.x_max)[0]
+
+    def predict(self, x):
+        Z = self.normalization_factor()
+        y = -(x - self.x_max) * (np.tanh(self.a * (x - self.x0)) + 1.0)
+        return y / Z
+
+    def log_likelihood(self, x, intensity):
+        return np.sum(intensity * np.log(self.predict(x) + self.eps))
+
+    def MLE(self, x, intensity):
+        def func(param):
+            self.x0 = param[0]
+            self.a = param[1]
+            return - self.log_likelihood(x, intensity)
+
+        init = [self.x0, self.a]
+        info = optimize.minimize(func, x0=init, bounds=[(self.x_min, self.x_max),
+                                                        (0, np.inf)], method='L-BFGS-B')
+        self.x0 = info['x'][0]
+        self.a = info['x'][1]
+
+        return info
+
+
+class TanhQuadModel:
+    def __init__(self, x_min=-10.0, x_max=10.0):
+        self.x_min = x_min
+        self.x_max = x_max
+        self.x0 = 0.0
+        self.a = 2.0
+        self.eps = 1e-10
+
+    def normalization_factor(self):
+        def func(x):
+            return -(x - self.x_max) * (np.tanh(self.a * (x - self.x0)) + 1.0)
+
+        return quad(func, self.x_min, self.x_max)[0]
+
+    def predict(self, x):
+        Z = self.normalization_factor()
+        y = (x - self.x_max) ** 2 * (np.tanh(self.a * (x - self.x0)) + 1.0)
+        return y / Z
+
+    def log_likelihood(self, x, intensity):
+        return np.sum(intensity * np.log(self.predict(x) + self.eps))
+
+    def MLE(self, x, intensity):
+        def func(param):
+            self.x0 = param[0]
+            self.a = param[1]
+            return - self.log_likelihood(x, intensity)
+
+        init = [self.x0, self.a]
+        info = optimize.minimize(func, x0=init, bounds=[(self.x_min, self.x_max),
+                                                        (0, np.inf)], method='L-BFGS-B')
+        self.x0 = info['x'][0]
+        self.a = info['x'][1]
+
+        return info
+
+
+class TanhCubicModel:
+    def __init__(self, x_min=-10.0, x_max=10.0):
+        self.x_min = x_min
+        self.x_max = x_max
+        self.x0 = 0.0
+        self.a = 2.0
+        self.eps = 1e-10
+
+    def normalization_factor(self):
+        def func(x):
+            return -(x - self.x_max) * (np.tanh(self.a * (x - self.x0)) + 1.0)
+
+        return quad(func, self.x_min, self.x_max)[0]
+
+    def predict(self, x):
+        Z = self.normalization_factor()
+        y = -(x - self.x_max) ** 3 * (np.tanh(self.a * (x - self.x0)) + 1.0)
+        return y / Z
+
+    def log_likelihood(self, x, intensity):
+        return np.sum(intensity * np.log(self.predict(x) + self.eps))
+
+    def MLE(self, x, intensity):
+        def func(param):
+            self.x0 = param[0]
+            self.a = param[1]
+            return - self.log_likelihood(x, intensity)
+
+        init = [self.x0, self.a]
+        info = optimize.minimize(func, x0=init, bounds=[(self.x_min, self.x_max),
+                                                        (0, np.inf)], method='L-BFGS-B')
+        self.x0 = info['x'][0]
+        self.a = info['x'][1]
+
+        return info
